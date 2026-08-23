f9e0935dec7ba05f74e391065ff8ef9a477ac492
Saeed Nemati <sn.1361@gmail.com>
2026-08-23 14:51:23 +0330

    committed by script

--- site/parts/attendanceStatus.jsx
+++ site/parts/attendanceStatus.jsx
@@ -1,4 +1,4 @@
-export default ({ attendanceRecord }) => <dl class='attendanceStatus'>
+export default ({ attendanceRecord }) => <dl class='status'>
     <dt class='workDate'>{attendanceRecord?.workDate}</dt>
     <dd class='checkInDate'>{attendanceRecord?.checkInDate}</dd>
     <dd class='checkOutDate'>{attendanceRecord?.checkOutDate}</dd>
