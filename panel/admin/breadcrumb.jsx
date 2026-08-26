8e4773cadec8c11397997c92f33997c31e91bfcf
Saeed Nemati <sn.1361@gmail.com>
2026-08-26 10:29:00 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='schedule'
+    name='attendance'
+    title='attendanceAttendance'
+    {...props}
+/>
